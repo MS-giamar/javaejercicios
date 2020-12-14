8f8cea5dccb5838dd26d1737d208295a28567022
Sofia Giamarini <sofiagiamarini@hotmail.com>
2020-12-14 13:04:30 -0300

    se agrega ejercicio 8 funcionando

--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -1,5 +1,5 @@
-var n1 = prompt("8");
-if (n1 % 2 === 0) {
+let numero1 = 8;
+if (numero1 % 2 === 0) {
     document.write("Es divisible por 2");
 } else {
     document.write("No es divisible por 2");
